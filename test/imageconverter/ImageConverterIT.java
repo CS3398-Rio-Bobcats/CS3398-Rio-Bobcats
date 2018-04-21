8c381db2f11dec7db6986b29b4fe3bb740fbc67f
Ary <ana80@txstate.edu>
2018-04-20 23:03:46 -0500

    aryDev

---
+++ test/imageconverter/ImageConverterIT.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imageconverter;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author aryamabebe
+ */
+public class ImageConverterIT {
+    
+    public ImageConverterIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testConvert() throws Exception {
+        System.out.println("convert");
+        String inputFilePath = "";
+        String format = "";
+        String expResult = "";
+        String result = ImageConverter.convert(inputFilePath, format);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetFileExtensionFromPath() {
+        System.out.println("getFileExtensionFromPath");
+        String path = "";
+        String expResult = "";
+        String result = ImageConverter.getFileExtensionFromPath(path);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetOutputPathFromInputPath() {
+        System.out.println("getOutputPathFromInputPath");
+        String path = "";
+        String format = "";
+        String expResult = "";
+        String result = ImageConverter.getOutputPathFromInputPath(path, format);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
